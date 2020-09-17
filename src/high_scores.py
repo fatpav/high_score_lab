ccd655dae06cd7004107e2fe21ca15666aa3ec30
fatpav <fat_pav@hotmail.com>
2020-09-17 14:05:46 +0100

    all tests written.  2 tests passed. 2 functions written

--- src/high_scores.py
+++ src/high_scores.py
@@ -3,9 +3,10 @@ def latest(scores):
     return scores[-1]
 
 
-def personal_best(scores):
-    pass
 
+def personal_best(scores):
+    return max(scores)
+    
 
 def personal_top_three(scores):
     pass
